404572a9022947655a847177b631b395d41efffe
nanobeauty <nanobeauty@naver.com>
2016-07-12 17:38:34 +0700

    Root (ngbook-master) 폴더에서 1. sudo npm install -g yo 2. sudo npm inatall -g generator-angular 3. mkdir samleApp && cd $_ 4. sampleApp 폴더에서 yo angular sampleApp 으로 프로젝트 폴더 생성    (gulp-미선택, grunt-선택, sass-미선택,  bootstrap-선택, 나머지 모듈 모두 선택 해제) 5. grunt server 후 localhost:9000 으로 접속

---
+++ sampleApp/app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name sampleAppApp
+ * @description
+ * # sampleAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('sampleAppApp', []);
